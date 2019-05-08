8128f3cf145e2dc8786a2c054cd11d4753ef3b8d
Rgelli10 <ricardo.gelli@hotmail.com>
2019-05-08 17:06:31 -0300

    delecao de usuario com delete

--- src/main/java/com/gelli/workshopmongo/services/UserService.java
+++ src/main/java/com/gelli/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return rep.insert(obj);
 	}
 	
+	public void delete(String id) {
+		rep.findById(id);
+		rep.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
