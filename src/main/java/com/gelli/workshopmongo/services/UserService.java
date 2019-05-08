b2a5949c5fb93082d2ec6cf78d9ed21f3bf61ec8
Rgelli10 <ricardo.gelli@hotmail.com>
2019-05-08 17:21:14 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/gelli/workshopmongo/services/UserService.java
+++ src/main/java/com/gelli/workshopmongo/services/UserService.java
@@ -35,6 +35,18 @@ public class UserService {
 		rep.deleteById(id);
 	}
 	
+	public User update (User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return rep.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
