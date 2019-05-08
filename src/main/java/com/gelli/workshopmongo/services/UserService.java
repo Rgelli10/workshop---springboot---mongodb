68d6422ffc881ddcca660a638c1875f3f23b909c
Rgelli10 <ricardo.gelli@hotmail.com>
2019-05-08 12:09:50 -0300

    Obtendo usuario por ID

--- src/main/java/com/gelli/workshopmongo/services/UserService.java
+++ src/main/java/com/gelli/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gelli.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gelli.workshopmongo.domain.User;
 import com.gelli.workshopmongo.repository.UserRepository;
+import com.gelli.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return rep.findAll();
 	}
-
+	
+	public User findById(String id) {
+		Optional<User> obj = rep.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
